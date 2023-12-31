c6d76acddbe408fa2c5aeb0e1acaa0c45119ac80
Guilherme Lino <lino000000@hotmail.com>
2023-12-31 00:12:00 +0000

    update

--- src/main/java/pt/ipleiria/estg/dei/ei/dae/daebackend/ejbs/ProdutoFisicoBean.java
+++ src/main/java/pt/ipleiria/estg/dei/ei/dae/daebackend/ejbs/ProdutoFisicoBean.java
@@ -3,8 +3,10 @@ package pt.ipleiria.estg.dei.ei.dae.daebackend.ejbs;
 import jakarta.ejb.Stateless;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
 import pt.ipleiria.estg.dei.ei.dae.daebackend.entities.Produto;
 import pt.ipleiria.estg.dei.ei.dae.daebackend.entities.ProdutoFisico;
+import pt.ipleiria.estg.dei.ei.dae.daebackend.exceptions.MyEntityNotFoundException;
 
 import java.util.List;
 
@@ -27,10 +29,15 @@ public class ProdutoFisicoBean {
                 .getResultList();
     }
 
-    public ProdutoFisico findFirstProdutoFisicoByProdutoId(int produtoId) {
-        return entityManager.createNamedQuery("getAllProdutosFisicosByProdutoId", ProdutoFisico.class)
+    public ProdutoFisico findFirstProdutoFisicoByProdutoId(int produtoId) throws MyEntityNotFoundException {
+        List<ProdutoFisico> query = entityManager.createNamedQuery("getAllProdutosFisicosByProdutoId", ProdutoFisico.class)
                 .setParameter("produtoId", produtoId)
-                .getSingleResult();
+                .getResultList();
+
+        if (query.isEmpty()) {
+            throw new MyEntityNotFoundException("Produto with produtoId: " + produtoId + " dont have stock");
+        }
+        return query.get(0);
     }
 
 
