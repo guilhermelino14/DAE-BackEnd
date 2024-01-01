b76503dfb9f2eab0a17feea060c9c3b28e16fbdb
Guilherme Lino <lino000000@hotmail.com>
2024-01-01 23:21:47 +0000

    Update ConfigBean.java

--- src/main/java/pt/ipleiria/estg/dei/ei/dae/daebackend/ejbs/ConfigBean.java
+++ src/main/java/pt/ipleiria/estg/dei/ei/dae/daebackend/ejbs/ConfigBean.java
@@ -57,7 +57,6 @@ public class ConfigBean {
         produtoFisicoBean.create(produtoBean.find(1));
         encomendaBean.create(operadorBean.find("operador1"), consumidorBean.find("consumidor1"));
         encomendaBean.addProduct(1, 1);
-        encomendaBean.create(operadorBean.find("operador1"), consumidorBean.find("consumidor1"));
         sensorBean.associarSensorAEmbalagem(1, 1);
         produtoFisicoBean.addEmbalagemProduto(1, 1);
     }
